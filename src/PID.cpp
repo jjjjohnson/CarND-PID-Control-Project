de96649c003d0c497dfe6a0a27907a0c78c02ae1
Junjie Jin <jjjjohnsonjin@gmail.com>
2017-06-01 16:24:00 +0800

    Fill pid class and main function

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,22 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double KP, double KI, double KD) {
+    Kp = KP;
+    Ki = KI;
+    Kd = KD;
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+    i_error += cte;
+    d_error = cte - p_error;
+    p_error = cte;
 }
 
 double PID::TotalError() {
+    return -1*(Kp*p_error + Ki*i_error + Kd*d_error);
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,10 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  double KP = 0.2;
+  double KI = 0.0001;
+  double KD = 1.0;
+  pid.Init(KP,KI,KD);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,7 +61,11 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+          if(steer_value > 1) steer_value = 1;
+          if(steer_value < -1) steer_value = -1;
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
